18ddc5adad9904c2a50cc9fbecf8fc1d330e8870
Jasper Ty <jasperty2@gmail.com>
2023-06-12 01:39:32 -0400

    Wrote gen_numbers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::ops::{ Index, IndexMut };
+use itertools::Itertools;
+
 use sdl2;
 use rand::seq::SliceRandom;
 use rand::{ thread_rng, Rng };
@@ -46,12 +49,43 @@ impl Board {
     }
 
     fn gen_numbers(&mut self) {
-
-        todo!();
+        let neighborhood = [
+            (-1, -1), (0, -1), (1, -1),
+            (-1,  0),          (1,  0),
+            (-1,  1), (0,  1), (1,  1),
+        ];
+        for (x, y) in (0..self.width).cartesian_product(0..self.height) {
+            if let Cell::Num(_) = self[(x, y)] {
+                let n: u8 = neighborhood.iter()
+                    .map(|(dx, dy)| (x as i32 + dx, y as i32 + dy))
+                    .filter(|(x, y)| 
+                        *x > 0 && *x < self.width as i32
+                        && *y > 0 && *y < self.height as i32)
+                    .map(|(x, y)| match self[(x as usize, y as usize)] {
+                        Cell::Mine => 1,
+                        _ => 0,
+                    })
+                    .sum();
+                self[(x, y)] = Cell::Num(n);
+            }
+        }
+    }
+}
+impl Index<(usize, usize)> for Board {
+    type Output = Cell;
+    fn index(&self, (x, y): (usize, usize)) -> &Self::Output {
+        &self.cells[y*self.width + x]
+    }
+}
+impl IndexMut<(usize, usize)> for Board {
+    fn index_mut(&mut self, (x, y): (usize, usize)) -> &mut Self::Output {
+        &mut self.cells[y*self.width + x]
     }
 }
 
 fn main() {
     let state = PlayState::IDLE;
 
+    let board = Board::new_random(30, 16, 99);
+    println!("{:?}", board.cells);
 }
