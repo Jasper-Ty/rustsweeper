78e1e8ed54b25a1fff46e898f185644700f1afcd
Jasper Ty <jasperty2@gmail.com>
2023-06-12 00:10:21 -0400

    Added Board struct and Cell enum

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,40 @@
 use sdl2;
+
+enum PlayState {
+    IDLE,
+    PLAY,
+    WIN,
+    GAMEOVER,
+}
+
+enum Cell {
+    MINE,
+    NUM(u8),
+}
+
+struct Board {
+    cells: Vec<Cell>,
+    width: usize,
+    height: usize,
+}
+
+impl Board {
+    pub fn new_random(width: usize, height: usize, num_mines: usize) -> Self {
+        let cells: Vec<Cell> = Vec::with_capacity(width * height);
+        todo!();
+    }
+
+    fn gen_mines(&mut self, num_mines: usize) {
+
+        todo!();
+    }
+    fn gen_numbers(&mut self, num_mines: usize) {
+
+        todo!();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let state = PlayState::IDLE;
+
 }
